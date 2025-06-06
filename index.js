b7124b911aae268b80fd357bb1240d32af3fe664
junior <junioristylo@gmail.com>
2025-06-06 12:49:22 -0300

    adcionando os links e resolvendo bugs

--- index.js
+++ index.js
@@ -31,9 +31,47 @@ overlay.addEventListener('click', () => {
 
 const verMaisButton = document.getElementById('verMais');
 const hiddenHaircuts = document.querySelectorAll('.hidden');
+let lastScrollPosition = 0; // Armazena a posição da rolagem
 
 verMaisButton.addEventListener('click', () => {
+    const isVerMais = verMaisButton.textContent === 'Ver Mais';
+
+    if (isVerMais) {
+        // Armazenar a posição atual da rolagem
+        lastScrollPosition = window.scrollY;
+    }
+
+    // Alternar a visibilidade dos elementos
     hiddenHaircuts.forEach(haircut => haircut.classList.toggle('hidden'));
-    verMaisButton.textContent =
-        verMaisButton.textContent === 'Ver Mais' ? 'Ver Menos' : 'Ver Mais';
+
+    // Alterar o texto do botão
+    verMaisButton.textContent = isVerMais ? 'Ver Menos' : 'Ver Mais';
+
+    if (!isVerMais) {
+        // Voltar para a posição armazenada ao recolher
+        window.scrollTo({
+            top: lastScrollPosition,
+            behavior: 'instant', // Rolagem suave para uma boa experiência
+        });
+    }
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Evita o comportamento padrão
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const offset = 180; // Ajuste conforme necessário
+            const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - offset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth' // Scroll suave
+            });
+        }
+    });
 });
